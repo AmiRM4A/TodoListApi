8e55eaf168b5bb27f7f4a73474983481b36fb7d0
Amir <asgry255@gmail.com>
2024-03-20 16:45:28 +0330

    :fire: Remove Echo Line

--- boot/init.php
+++ boot/init.php
@@ -1,3 +1,11 @@
 <?php
 
-echo "API";
+# Include required files
+require '../vendor/autoload.php';
+require './config.php';
+require './autoloader.php';
+require PROJ_DIR . str_replace('/', DIRECTORY_SEPARATOR, '/App/Helpers/helpers.php');
+
+# Loading dotenv variables
+$dotenv = new Symfony\Component\Dotenv\Dotenv();
+$dotenv->load(__DIR__ . '/.env');
