0095e3cef0bf6b29bf1de1d5d029bbce5ab2547a
Amir <asgry255@gmail.com>
2024-03-24 03:26:27 +0330

    :sparkles: Add Route Class

---
+++ App/Router/Route.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Router;
+
+use App\Helpers\Str;
+
+class Route {
+	private static $routes = [];
+
+	public static function add(string $name, array|string $method, callable|array|string $action): void {
+		static::$routes[$method] = [$name, Str::toUpperCase($method), $action];
+	}
+
+	public static function get(string $name, callable|array|string $action): void {
+		static::add($name, 'GET', $action);
+	}
+
+	public static function post(string $name, callable|array|string $action): void {
+		static::add($name, 'POST', $action);
+	}
+
+	public static function put(string $name, callable|array|string $action): void {
+		static::add($name, 'PUT', $action);
+	}
+
+	public static function delete(string $name, callable|array|string $action): void {
+		static::add($name, 'DELETE', $action);
+	}
+
+	public static function patch(string $name, callable|array|string $action): void {
+		static::add($name, 'PATCH', $action);
+	}
+
+	public static function getRoutes(): array {
+		return static::$routes;
+	}
+}
