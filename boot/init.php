00171b10a3a6fa3f08679aea1356095998be61ea
Amir <asgry255@gmail.com>
2024-03-24 03:25:08 +0330

    :sparkles: Add Security Helper
    (cherry picked from commit 2dc74618000d8bbed048daa2837ce82e6284fe6e)

--- boot/init.php
+++ boot/init.php
@@ -5,6 +5,7 @@ require '../vendor/autoload.php';
 require './config.php';
 require './autoloader.php';
 require PROJ_DIR . str_replace('/', DIRECTORY_SEPARATOR, '/App/Helpers/helpers.php');
+require PROJ_DIR . str_replace('/', DIRECTORY_SEPARATOR, '/App/Helpers/Security.php');
 
 # Loading dotenv variables
 $dotenv = new Symfony\Component\Dotenv\Dotenv();
