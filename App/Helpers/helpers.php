a22acdf6bab9fdd72e7ea71caf66229f093130a7
Amir <asgry255@gmail.com>
2024-03-28 17:41:50 +0330

    :zap: Improve Dump Function Structure
    Replacing print_r with var_dump to use xdebug stack trace and also a \n for haivng a new line after each dump

--- App/Helpers/helpers.php
+++ App/Helpers/helpers.php
@@ -1,14 +1,17 @@
 <?php
 
 if (!function_exists('dump')) {
-	function dump($data): void {
+	function dump($output): void {
 		$trace = debug_backtrace();
 		$caller = array_shift($trace);
 
 		echo '<pre style="background-color: #f0f0f0;border: 1px solid #ddd;padding: 10px;font-family: monospace;font-size: 14px;border-radius: 10px;border-left: 7px solid orange;">';
 		echo '<strong>File:</strong> ' . $caller['file'] . ' <strong>Line:</strong> ' . $caller['line'] . "\n\n";
-		print_r($data);
-		echo '</pre>';
+		var_dump($output);
+		echo '</pre>' . "\n";
+	}
+}
+
 if (!function_exists('dd')) {
 	function dd($output): void {
 		dump($output);
