df8261765886c753e6d563af9ccb7b42b8f300b3
Amir <asgry255@gmail.com>
2024-03-28 17:39:31 +0330

    :art: Replace Static With Self in Method Buddies

--- App/Router/Route.php
+++ App/Router/Route.php
@@ -16,26 +16,26 @@ class Route {
 	}
 
 	public static function get(string $name, callable|array|string $action): void {
-		static::add($name, 'GET', $action);
+		self::add($name, 'GET', $action);
 	}
 
 	public static function post(string $name, callable|array|string $action): void {
-		static::add($name, 'POST', $action);
+		self::add($name, 'POST', $action);
 	}
 
 	public static function put(string $name, callable|array|string $action): void {
-		static::add($name, 'PUT', $action);
+		self::add($name, 'PUT', $action);
 	}
 
 	public static function delete(string $name, callable|array|string $action): void {
-		static::add($name, 'DELETE', $action);
+		self::add($name, 'DELETE', $action);
 	}
 
 	public static function patch(string $name, callable|array|string $action): void {
-		static::add($name, 'PATCH', $action);
+		self::add($name, 'PATCH', $action);
 	}
 
 	public static function getRoutes(): array {
-		return static::$routes;
+		return self::$routes;
 	}
 }
