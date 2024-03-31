45090be71f862a47688ca5d55811b76ea2763927
Amir <asgry255@gmail.com>
2024-03-31 18:54:51 +0330

    :art: Improve init.php File Structure & Clean Code

--- boot/init.php
+++ boot/init.php
@@ -4,8 +4,13 @@ use App\Router\Router;
 
 # Include required files
 require '../vendor/autoload.php';
-require './config.php';
 require './autoloader.php';
+
+# Loading dotenv variables
+$dotenv = new Symfony\Component\Dotenv\Dotenv();
+$dotenv->load(__DIR__ . '/.env');
+
+require './config.php';
 require PROJ_DIR . str_replace('/', DIRECTORY_SEPARATOR, '/App/Helpers/helpers.php');
 require PROJ_DIR . str_replace('/', DIRECTORY_SEPARATOR, '/App/Helpers/Security.php');
 require PROJ_DIR . str_replace('/', DIRECTORY_SEPARATOR, '/App/Helpers/Str.php');
@@ -13,9 +18,5 @@ require PROJ_DIR . str_replace('/', DIRECTORY_SEPARATOR, '/App/Helpers/Str.php')
 # Routes
 require PROJ_DIR . str_replace('/', DIRECTORY_SEPARATOR, '/App/Router/Routes/api.php');
 
-# Loading dotenv variables
-$dotenv = new Symfony\Component\Dotenv\Dotenv();
-$dotenv->load(__DIR__ . '/.env');
-
-# Routing 
+# Routing
 Router::dispatch();
