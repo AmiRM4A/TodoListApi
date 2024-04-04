956c9686465fabe29ec8e0d0d98388af3aa7288c
Amir <asgry255@gmail.com>
2024-04-04 19:33:13 +0330

    :zap: Improve Autoloader
    Use PROJ_DIR const

--- boot/autoloader.php
+++ boot/autoloader.php
@@ -1,7 +1,7 @@
 <?php
 
 spl_autoload_register(function ($className) {
-	$filePath = str_replace(['/', '\\'], DIRECTORY_SEPARATOR, dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . $className . '.php');
+	$filePath = str_replace(['/', '\\'], DIRECTORY_SEPARATOR, PROJ_DIR . DIRECTORY_SEPARATOR . $className . '.php');
 	if (file_exists($filePath) && is_readable($filePath)) {
 		require $filePath;
 	} else {
