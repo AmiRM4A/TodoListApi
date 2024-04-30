6ab63cf6ff426fd7d9a11b5d1f724c31b9339481
Amir <asgry255@gmail.com>
2024-04-30 15:25:13 +0330

    :sparkles: Add ApiRoute & CORS

---
+++ App/Router/ApiRoute.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Router;
+
+/**
+ * ApiRoute class
+ *
+ * The ApiRoute class is a subclass of the Route class and is designed to handle API routes
+ * in a web application. It automatically applies the CORS (Cross-Origin Resource Sharing)
+ * middleware to all API routes.
+ */
+class ApiRoute extends Route {
+	/**
+	 * Route constructor.
+	 *
+	 * @param string $route The route path.
+	 * @param array|string $method The HTTP methods allowed for this route.
+	 * @param callable|array|string $action The action to be performed when the route is matched.
+	 * @param array|string $middleware An array or string of middleware classes/functions to be executed before the route action.
+	 */
+	public function __construct(string $route, array|string $method, callable|array|string $action, array|string $middleware = []) {
+		// Call the parent constructor with the provided parameters
+		parent::__construct($route, $method, $action, $middleware);
+		
+		// Apply the CORS middleware to the route
+		$this->middleware(\App\Middlewares\CORS::class);
+	}
+}
