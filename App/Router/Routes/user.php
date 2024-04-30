d6e4918c5decff38070b22ef42284f0ff929b5ac
Amir <asgry255@gmail.com>
2024-04-30 15:28:14 +0330

    :art: Apply ApiRoute to Routes (Replace)
    Replaced with Route class (For adding default middlewares and...)

--- App/Router/Routes/user.php
+++ App/Router/Routes/user.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Router\Route;
+use App\Router\ApiRoute;
 
-Route::get('get-users', 'UserController@index'); // Get all users
-Route::get('get-user/{id}', 'UserController@show'); // Get a specific user by id
-Route::post('create-user', 'UserController@create'); // Create new user
-Route::delete('remove-user/{id}', 'UserController@destroy'); // Delete an existing user
-Route::put('update-user/{id}', 'UserController@update'); // Update an existing user
+ApiRoute::get('get-users', 'UserController@index'); // Get all users
+ApiRoute::get('get-user/{id}', 'UserController@show'); // Get a specific user by id
+ApiRoute::post('create-user', 'UserController@create'); // Create new user
+ApiRoute::delete('remove-user/{id}', 'UserController@destroy'); // Delete an existing user
+ApiRoute::put('update-user/{id}', 'UserController@update'); // Update an existing user
